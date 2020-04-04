fe6a6a12b7fd411445c0d394ff547308a26d44fa
Matheus Cavalcante <matheusfernandes3m@hotmail.com>
2020-04-03 22:26:56 -0300

    Coloquei Scroll pra quando abre o teclado

--- appcontabil/lib/pages/login.page.dart
+++ appcontabil/lib/pages/login.page.dart
@@ -1,157 +1,195 @@
+import 'package:appcontabil/animation/FadeAnimation.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
       body: Container(
-        padding: EdgeInsets.symmetric(vertical: 30),
         width: double.infinity,
         decoration: BoxDecoration(
-          gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-            Colors.deepPurple[900],
-            Colors.deepPurple[800],
-            Colors.deepPurple[400],
-          ]),
-        ),
+            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+          Colors.deepPurple[900],
+          Colors.deepPurple[800],
+          Colors.deepPurple[400]
+        ])),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             SizedBox(
               height: 80,
             ),
             Padding(
               padding: EdgeInsets.all(20),
-              child: Column( 
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text("Seja Bem vindo!",
-                    style: TextStyle(color: Colors.white, fontSize: 30),
-                    ),
+                children: <Widget>[
+                  FadeAnimation(
+                      1,
+                      Text(
+                        "EA ASSISTANT",
+                        style: TextStyle(color: Colors.white, fontSize: 40),
+                      )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  FadeAnimation(
+                      1.3,
+                      Text(
+                        "AQUI VAI FICAR A LOGO",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      )),
                 ],
               ),
             ),
+            SizedBox(height: 20),
             Expanded(
               child: Container(
-                child: Padding(
-                  padding: EdgeInsets.all(30),
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                          color: Colors.white,                          
-                          borderRadius: BorderRadius.circular(5),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(60),
+                        topRight: Radius.circular(60))),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(
+                          height: 60,
                         ),
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              padding: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                border: Border(
-                                  bottom: BorderSide(color: Colors.grey[200]),
-                                ),
-                              ),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                  hintText: "Email",
-                                  hintStyle: TextStyle(color: Colors.grey),
-                                  border: InputBorder.none,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              padding: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                border: Border(
-                                  bottom: BorderSide(color: Colors.grey[200]),
-                                ),
-                              ),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                  hintText: "Senha",
-                                  hintStyle: TextStyle(color: Colors.grey),
-                                  border: InputBorder.none,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(height: 40),
-                      Text(
-                        "Esqueceu sua senha?",
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                      SizedBox(height: 40),
-                      Container(
-                        height: 50,
-                        margin: EdgeInsets.symmetric(horizontal: 50),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          color: Colors.deepPurple[900],
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Login",
-                            style: TextStyle(
+                        FadeAnimation(
+                          1.4,
+                          Container(
+                            decoration: BoxDecoration(
                                 color: Colors.white,
-                                fontWeight: FontWeight.bold),
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Color.fromRGBO(225, 95, 27, .3),
+                                      blurRadius: 20,
+                                      offset: Offset(0, 10))
+                                ]),
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                      border: Border(
+                                          bottom: BorderSide(
+                                              color: Colors.grey[200]))),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                        hintText: "Email",
+                                        hintStyle:
+                                            TextStyle(color: Colors.grey),
+                                        border: InputBorder.none),
+                                  ),
+                                ),
+                                Container(
+                                  padding: EdgeInsets.all(10),
+                                  decoration: BoxDecoration(),
+                                  child: TextField(
+                                    decoration: InputDecoration(
+                                        hintText: "Senha",
+                                        hintStyle:
+                                            TextStyle(color: Colors.grey),
+                                        border: InputBorder.none),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-
-                      SizedBox(height: 30),
-                      Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: Container(
+                        SizedBox(
+                          height: 40,
+                        ),
+                        FadeAnimation(
+                            1.5,
+                            Text(
+                              "Esqueceu a senha?",
+                              style: TextStyle(color: Colors.grey),
+                            )),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        FadeAnimation(
+                            1.6,
+                            Container(
                               height: 50,
+                              margin: EdgeInsets.symmetric(horizontal: 50),
                               decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50),
-                                color: Colors.blue,
-                              ),
+                                  borderRadius: BorderRadius.circular(50),
+                                  color: Colors.deepPurple[900]),
                               child: Center(
                                 child: Text(
-                                  "Facebook",
+                                  "Login",
                                   style: TextStyle(
                                       color: Colors.white,
                                       fontWeight: FontWeight.bold),
                                 ),
                               ),
+                            )),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        FadeAnimation(
+                            1.7,
+                            Text(
+                              "Login pelas redes sociais",
+                              style: TextStyle(color: Colors.grey),
+                            )),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: FadeAnimation(
+                                  1.8,
+                                  Container(
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(50),
+                                        color: Colors.blue),
+                                    child: Center(
+                                      child: Text(
+                                        "Facebook",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  )),
                             ),
-                          ),
-                          SizedBox(
-                            width: 30,
-                          ),
-                          Expanded(
-                            child: Container(
-                              height: 50,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50),
-                                color: Colors.black,
-                              ),
-                              child: Center(
-                                child: Text(
-                                  "Github",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
+                            SizedBox(
+                              width: 30,
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
+                            Expanded(
+                              child: FadeAnimation(
+                                  1.9,
+                                  Container(
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(50),
+                                        color: Colors.black),
+                                    child: Center(
+                                      child: Text(
+                                        "Github",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  )),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
+            )
           ],
         ),
       ),
