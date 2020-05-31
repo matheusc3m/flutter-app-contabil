d8c5de98ca0350a9c3cac77903242ac87972277d
matheusc3m <matheusfernandes3m@hotmail.com>
2020-05-30 21:07:35 -0300

    Filtragem por mês na dashboard

--- appcontabil/lib/pages/dashboard.dart
+++ appcontabil/lib/pages/dashboard.dart
@@ -4,6 +4,7 @@ import 'package:appcontabil/animation/FadeAnimation.dart';
 import 'package:appcontabil/models/lancamento_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class DashboardPage extends StatefulWidget {
   @override
@@ -11,12 +12,61 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  var dataAtual = new DateTime.now();
+
+  CalendarController calendarController;
+  String dataFormatada;
+  var dataInicial;
+  DateTime dataFinal;
+  Future retornaFuture() async {
+    var firestore = Firestore.instance;
+    QuerySnapshot qn;
+    if (dataInicial != null) {
+      lancamentoInscricao = db
+          .collection("lancamento")
+          .where("date",
+              isGreaterThanOrEqualTo: Timestamp.fromDate(dataInicial))
+          .where("date", isLessThan: Timestamp.fromDate(dataFinal))
+          .snapshots()
+          .listen((snapshot) {
+        final List<Lancamento> lancamentos = snapshot.documents
+            .map(
+              (documentSnapshot) => Lancamento.fromMap(
+                  documentSnapshot.data, documentSnapshot.documentID),
+            )
+            .toList();
+        items = lancamentos;
+      });
+      qn = await firestore
+          .collection("lancamento")
+          .where("date",
+              isGreaterThanOrEqualTo: Timestamp.fromDate(dataInicial))
+          .where("date",
+              isLessThanOrEqualTo: Timestamp.fromDate(dataFinal.toUtc()))
+          .getDocuments();
+      qn.documents.asMap();
+    } else {
+      qn = await firestore
+          .collection("lancamento")
+          .where("date", isLessThanOrEqualTo: Timestamp.fromDate(dataAtual))
+          .getDocuments();
+    }
+    return qn;
+  }
+
   Lancamento l = Lancamento();
   List<Lancamento> items;
   var db = Firestore.instance;
   StreamSubscription<QuerySnapshot> lancamentoInscricao;
+
   @override
   void initState() {
+    dataInicial = new DateTime(dataAtual.year, (dataAtual.month), 1, 0);
+    dataFinal = new DateTime(dataAtual.year, (dataAtual.month + 1), 0);
+    print("DataFinal $dataFinal");
+    print("DataInicial $dataInicial");
+    super.initState();
+    calendarController = CalendarController();
     super.initState();
 
     items = List();
@@ -86,53 +136,101 @@ class _DashboardPageState extends State<DashboardPage> {
               )),
             ),
           ),
+          TableCalendar(
+            calendarController: calendarController,
+            locale: "pt_BR",
+            headerStyle: HeaderStyle(
+              titleTextStyle: TextStyle(fontSize: 18, color: Colors.black),
+              formatButtonShowsNext: false,
+              formatButtonVisible: false,
+              centerHeaderTitle: true,
+            ),
+            calendarStyle: CalendarStyle(
+                outsideDaysVisible: false, contentPadding: EdgeInsets.all(0)),
+            daysOfWeekStyle: DaysOfWeekStyle(
+              weekdayStyle: TextStyle(color: Colors.transparent),
+              weekendStyle: TextStyle(color: Colors.transparent),
+            ),
+            rowHeight: 0,
+            initialCalendarFormat: CalendarFormat.month,
+            onVisibleDaysChanged: (dateFirst, dateLast, CalendarFormat cf) {
+              print(dateFirst);
+              setState(() {
+                dataInicial = dateFirst;
+                dataFinal = dateLast;
+              });
+
+              print("DATA FORMATADA CALENDAR $dataFormatada");
+
+              //print("Data Inicial: $dateFirst ....... Data Final: $dateLast");
+            },
+          ),
           Expanded(
-            child: GridView.builder(
-                padding: EdgeInsets.all(10),
-                itemCount: array.length,
-                gridDelegate: (SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 1,
-                  childAspectRatio: MediaQuery.of(context).size.width /
-                      (MediaQuery.of(context).size.height / 4.3),
-                )),
-                itemBuilder: (BuildContext context, int index) {
-                  return FadeAnimation(
-                    1.2,
-                    Card(
-                      color: _getMyColor(tipo[index]),
-                      child: InkWell(
-                        onTap: () {},
-                        child: Column(
-                          children: <Widget>[
-                            SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.01),
-                            Padding(
-                              padding: EdgeInsets.all(20),
-                              child: Text(
-                                menu[index],
-                                style: TextStyle(
-                                    fontSize:
-                                        MediaQuery.of(context).size.height *
-                                            0.03,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w400,
-                                    height: 1.2),
-                                textAlign: TextAlign.center,
+            child: FutureBuilder(
+                /*meus dados*/ future: retornaFuture(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    default:
+                      List<DocumentSnapshot> documentos =
+                          snapshot.data.documents;
+                      return GridView.builder(
+                          padding: EdgeInsets.all(10),
+                          itemCount: array.length,
+                          gridDelegate:
+                              (SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 1,
+                            childAspectRatio:
+                                MediaQuery.of(context).size.width /
+                                    (MediaQuery.of(context).size.height / 4.3),
+                          )),
+                          itemBuilder: (BuildContext context, int index) {
+                            return FadeAnimation(
+                              1.2,
+                              Card(
+                                color: _getMyColor(tipo[index]),
+                                child: InkWell(
+                                  onTap: () {},
+                                  child: Column(
+                                    children: <Widget>[
+                                      SizedBox(
+                                          height: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.01),
+                                      Padding(
+                                        padding: EdgeInsets.all(20),
+                                        child: Text(
+                                          menu[index],
+                                          style: TextStyle(
+                                              fontSize: MediaQuery.of(context)
+                                                      .size
+                                                      .height *
+                                                  0.03,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w400,
+                                              height: 1.2),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                      Text(
+                                        "R\$ ${array[index]}",
+                                        style: TextStyle(
+                                            fontSize: 19,
+                                            color: Colors.white60,
+                                            fontWeight: FontWeight.w700),
+                                      )
+                                    ],
+                                  ),
+                                ),
                               ),
-                            ),
-                            Text(
-                              "R\$ ${array[index]}",
-                              style: TextStyle(
-                                  fontSize: 19,
-                                  color: Colors.white60,
-                                  fontWeight: FontWeight.w700),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
+                            );
+                          });
+                  }
                 }),
           ),
         ],
